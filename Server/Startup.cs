54162216a5a9191a42b34b8ddce8ade2233ed0e8
mg-emcom-dk <55033278+mg-emcom-dk@users.noreply.github.com>
2022-10-08 15:11:05 +0200

    fix the project to run

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -33,7 +33,7 @@ namespace CarChecker.Server
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false ) //disable confirm email for dev
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
